e79318974f4afe877f43160cd15dca6ba0568481
Andri Rakotomalala <andriniaina@gmail.com>
2019-04-11 23:59:47 +0200

    up

--- cv/generate.sh
+++ cv/generate.sh
@@ -1,4 +1,5 @@
-pandoc -t html5 --mathjax -s "--include-before-body=_header.html" "--include-before-body=_beforedoc.html" "--include-after-body=_footer.html" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" "--css=custom.css" ./cv.markdown -o cv.html
-pandoc -V geometry:margin=1in ./cv.markdown -o cv_default_style.pdf
-#pandoc -f gfm ./cv.markdown -o cv_gfm.pdf
-#pandoc -f markdown_mmd ./cv.markdown -o cv_mmd.pdf
+pandoc -t html5 --mathjax -s "--include-before-body=_beforebody.html" "--include-after-body=_footer.html" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" "--css=custom.css" ./cv_fr.markdown -o cv.html
+pandoc -V geometry:margin=1in ./cv_fr.markdown -o cv_default_style_en.pdf
+
+pandoc -t html5 --mathjax -s "--include-before-body=_beforebody.html" "--include-after-body=_footer.html" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" "--css=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" "--css=custom.css" ./cv_en.markdown -o cv_en.html
+pandoc -V geometry:margin=1in ./cv_en.markdown -o cv_default_style_fr.pdf
